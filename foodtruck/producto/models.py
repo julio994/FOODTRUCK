4d033a12c1edb47fc3de837a3365eefbd14a723f
“julio994” <julio994@utp.edu.co>
2024-04-16 01:33:34 -0500

    preparacion de aplicacion producto

--- foodtruck/producto/models.py
+++ foodtruck/producto/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Producto(models.Model):
     name=models.CharField(max_length=200, verbose_name="nombre")
     price=models.IntegerField(verbose_name="precio")
     image= models.ImageField(verbose_name="Imagen", upload_to="imagen")
-
+    created= models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated= models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion" )
 
---
+++ foodtruck/producto/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('',views.producto , name="producto_page"),
+    
+]
