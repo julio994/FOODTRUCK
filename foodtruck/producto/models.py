b04e947582ca8d21651a14adc2f376ba880a3a81
Luis Hernandez <luishernandez@utp.edu.co>
2024-04-16 02:18:23 -0500

    integracion producto y restaurante

--- foodtruck/producto/models.py
+++ foodtruck/producto/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from restaurante.models import Restaurante
 
 class Producto(models.Model):
     name=models.CharField(max_length=200, verbose_name="nombre")
@@ -8,4 +8,6 @@ class Producto(models.Model):
     image= models.ImageField(verbose_name="Imagen", upload_to="imagen")
     created= models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
     updated= models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion" )
+    id_restaurante = models.ForeignKey(Restaurante,verbose_name="Restaurante", on_delete=models.CASCADE)
+    # foranea
 
