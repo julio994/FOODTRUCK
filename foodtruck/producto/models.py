f5f8c7ee16026682ce7cc6c0ae0e20652e9ddaf8
“julio994” <julio994@utp.edu.co>
2024-04-16 00:16:31 -0500

    creacion modelo

--- foodtruck/producto/models.py
+++ foodtruck/producto/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    name=models.CharField(max_length=200, verbose_name="nombre")
+    price=models.IntegerField(verbose_name="precio")
+    image= models.ImageField(verbose_name="Imagen", upload_to="imagen")
+
+
